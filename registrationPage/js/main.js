fd3043305095ae18a5a4df647bc6ccc96c8b5e07
Yevhenii <hikienkiller@gmail.com>
2024-06-17 01:11:08 -0300

    version 0.2 litle puch

--- registrationPage/js/main.js
+++ registrationPage/js/main.js
@@ -3,14 +3,14 @@ const  playbtn = document.querySelector('.playBtn');
 const  vinilRecorderIcon = document.querySelector('.gameIcon');
 
 
-playbtn.addEventListener('click',addClasshtmlElm('active',vinilRecorderIcon,playbtn,4,5));
+playbtn.addEventListener('click',addClasshtmlElm('active',vinilRecorderIcon,playbtn,));
 
 
 
 function addClasshtmlElm(className,...element){
     return (e) =>{
             element.forEach((elem)=>{
-            
+            console.log(elem.className.split(" ")[0]);
         })
     }
 }
